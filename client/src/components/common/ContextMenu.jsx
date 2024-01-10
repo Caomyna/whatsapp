844f6d0dde4c6509aa24451b7b84fb4d38fb2a95
Caomyna <caomyna03@gmail.com>
2024-01-10 14:58:28 +0700

    update 1:32

--- client/src/components/common/ContextMenu.jsx
+++ client/src/components/common/ContextMenu.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-function ContextMenu() {
-  return <div>ContextMenu</div>;
+function ContextMenu({options, cordinates, contextMenu, setContextMenu}) {
+  const contextMenuRef = useRef(null);
+  return (
+    <div className={`bg-dropdown-background fixed py-2 z-[100] top-[${cordinates.y} left-[${cordinates.x}] shadow-xl`}
+    ref={contextMenuRef}
+    
+  >
+    <ul>
+      {
+        options.map(({name,callback })=>(
+          <li key={name} onClick={()=>handleClick(e,callback)}>
+
+          </li>
+        ))
+
+      }
+    </ul>
+
+  </div>
+  );
 }
 
 export default ContextMenu;
--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import Image from "next/image";
 import Input from "@/components/common/Input";
 import { useStateProvider } from "@/context/StateContext";
+import Avatar from "@/components/common/Avatar";
 
 function onboarding() {
   const [{ userInfo }] = useStateProvider();
@@ -23,9 +24,13 @@ function onboarding() {
       <h2 className="text-2xl">Create your profile</h2>
       <div className="flex gap-6 mt-6">
         <div className="flex flex-col items-center justify-center mt-5 gap-6">
-          {/* {userInfo.name} */}
+          
           <Input name="Display Name" state={name} setState={setName} label />
           <Input name="About" state={about} setState={setAbout} label />
+        </div>
+
+        <div>
+          <Avatar type="xl" image={image} setImage={setImage}/>
         </div>
       </div>
     </div>
